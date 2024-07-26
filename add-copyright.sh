a2feaba5194f7ec1e78a9ef89c76cc49f297cfd2
oimasterkafuu <oimasterfake@icloud.com>
2024-07-26 20:07:04 +0800

    feat: add copyright notice to js, html, and css files

--- add-copyright.sh
+++ add-copyright.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# 要添加版权提示的文件扩展名
-EXT="js"
+# 要添加版权提示的文件扩展名列表
+EXTENSIONS=("js" "html" "css")
 
-# 版权提示内容
-COPYRIGHT_NOTICE="/*
-Chat - A simple chat application.
+# 版权声明文本
+COPYRIGHT_TEXT="Chat - A simple chat application.
 Copyright (C) 2024 oimaster
 
 This program is free software: you can redistribute it and/or modify
@@ -15,35 +14,46 @@ the Free Software Foundation, either version 3 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 Contact Information:
 Email: oimasterfake@icloud.com
 
-Project URL: https://github.com/oimasterkafuu/chat
-*/
+Project URL: https://github.com/oimasterkafuu/chat"
 
-"
+# 注释模板
+COMMENT_JS="/*\n$COPYRIGHT_TEXT\n*/\n"
+COMMENT_HTML="<!--\n$COPYRIGHT_TEXT\n-->\n"
+COMMENT_CSS="/*\n$COPYRIGHT_TEXT\n*/\n"
 
 # 遍历所有指定扩展名的文件，忽略node_modules目录
-find . -type f -name "*.$EXT" ! -path "./node_modules/*" | while read FILE; do
-    # 检查文件是否已经包含版权提示
-    if grep -q "Copyright (C) 2024 oimaster" "$FILE"; then
-        echo "Copyright notice already exists in $FILE"
-    else
-        # 临时文件
-        TEMP_FILE=$(mktemp)
+for EXT in "${EXTENSIONS[@]}"; do
+    find . -type f -name "*.$EXT" ! -path "./node_modules/*" | while read FILE; do
+        # 选择适当的注释模板
+        case "$EXT" in
+            js) COPYRIGHT_NOTICE="$COMMENT_JS" ;;
+            html) COPYRIGHT_NOTICE="$COMMENT_HTML" ;;
+            css) COPYRIGHT_NOTICE="$COMMENT_CSS" ;;
+        esac
 
-        # 将版权提示和原文件内容合并到临时文件
-        echo "$COPYRIGHT_NOTICE" | cat - "$FILE" > "$TEMP_FILE"
+        # 检查文件是否已经包含版权提示
+        if grep -q "Copyright (C) 2024 oimaster" "$FILE"; then
+            echo "Copyright notice already exists in $FILE"
+        else
+            # 临时文件
+            TEMP_FILE=$(mktemp)
 
-        # 将临时文件内容覆盖到原文件
-        mv "$TEMP_FILE" "$FILE"
+            # 将版权提示和原文件内容合并到临时文件
+            echo -e "$COPYRIGHT_NOTICE" | cat - "$FILE" > "$TEMP_FILE"
 
-        echo "Added copyright notice to $FILE"
-    fi
+            # 将临时文件内容覆盖到原文件
+            mv "$TEMP_FILE" "$FILE"
+
+            echo "Added copyright notice to $FILE"
+        fi
+    done
 done
