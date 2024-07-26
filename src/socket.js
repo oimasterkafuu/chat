1887ea7d33943b6cfb7a8c2ead8fb982c83a2d05
oimasterkafuu <oimasterfake@icloud.com>
2024-07-26 20:48:12 +0800

    feat: private message

--- src/socket.js
+++ src/socket.js
@@ -22,22 +22,51 @@ Project URL: https://github.com/oimasterkafuu/chat
 */
 
 
+const clientNumbers = {};
+
 module.exports = (io) => {
     // 监听客户端连接
     io.on('connection', (socket) => {
         console.log(`Client ${socket.id} connected`);
-    
+
+        // 赋予随机六位数编号
+        const number = Math.floor(Math.random() * (999999 - 100000) + 100000);
+        clientNumbers[socket.id] = number;
+        console.log(`Client ${socket.id} number: ${number}`);
+        socket.emit('number', number);
+
         // 监听客户端发送的消息
-        socket.on('message', (message) => {
-            console.log(`Client ${socket.id} sent a message: ${message}`);
-    
-            // 将消息发送给所有客户端
-            io.emit('message', message);
+        socket.on('message', (data) => {
+            const text = data.text, receiver = parseInt(data.receiver);
+            console.log(`Client ${clientNumbers[socket.id]} -> ${receiver}: ${text}`);
+
+            // 找到接收方
+            var receiverSocket = null;
+
+            for (const clientId of Object.keys(clientNumbers)) {
+                if (clientNumbers[clientId] === receiver) {
+                    receiverSocket = clientId;
+                    break;
+                }
+            }
+
+            if (receiverSocket) {
+                console.log(`Client ${clientNumbers[socket.id]} -> ${clientNumbers[receiverSocket.id]}: ${text}`);
+
+                io.to(receiverSocket).emit('message', text);
+
+                // 发送成功
+                socket.emit('ok');
+            } else {
+                // 发送失败
+                socket.emit('fail', '对方不在线');
+            }
         });
-    
+
         // 监听客户端断开连接
         socket.on('disconnect', () => {
             console.log(`Client ${socket.id} disconnected`);
+            delete clientNumbers[socket.id];
         });
     });
 };
